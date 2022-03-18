521b974265b1fc4b347d619383adee28ed86ada4
Benjamin De Leener <benjamin.de-leener@polymtl.ca>
2022-03-18 13:49:01 -0400

    Décorateur

--- time_simple_exemple.py
+++ time_simple_exemple.py
@@ -1,17 +1,26 @@
 from time import time
 
+def temps_repeat(repetitions):
+    def decorateur_temps(func):
+        def wrapper(*args, **kwargs):
+            temps = []
+            for _ in range(repetitions):
+                begin = time()
+                resultat = func(*args, **kwargs)
+                end = time()
+                temps.append(end-begin)
+            print(f'Cela a pris {sum(temps)/repetitions} secondes!')
+            return resultat
+        return wrapper
+    return decorateur_temps
 
+@temps_repeat(repetitions=100)
 def some_function():
-    some_list = [i for i in range(10000)]
+    some_list = [i for i in range(1000)]
 
     new_list = []
     for elem in some_list:
         new_list.append([i for i in range(elem)])
 
 
-if __name__ == "__main__":
-    begin = time()
-    some_function()
-    end = time()
-
-    print(f'Cela a pris {end - begin} secondes!')
+some_function()
